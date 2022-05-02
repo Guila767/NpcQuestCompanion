e96c077177b6c0c7d8b6642fe08174c532450ef8
Guilherme de Massena Ladario <31376051+Guila767@users.noreply.github.com>
2022-05-01 23:03:47 -0300

    drawer widget

---
+++ lib/src/drawer/drawer_tile.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import '../app_colors.dart';
+
+class DrawerTile extends StatelessWidget {
+  final String title;
+  final IconData? icon;
+  final void Function()? onTap;
+  final bool isSelected;
+  final Color iconColor;
+
+  const DrawerTile(
+      {required this.title,
+      this.icon,
+      this.onTap,
+      this.isSelected = false,
+      this.iconColor = Colors.black,
+      Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+          margin: const EdgeInsets.only(top: 6),
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                    color: isSelected
+                        ? Colors.black.withOpacity(0.1)
+                        : Colors.transparent,
+                    offset: const Offset(0, 0.5),
+                    spreadRadius: 3,
+                    blurRadius: 10)
+              ],
+              color: AppColors.blue700,
+              borderRadius: const BorderRadius.all(Radius.circular(54))),
+          child: Row(
+            children: [
+              Icon(
+                icon,
+                size: 18,
+                color: isSelected
+                    ? AppColors.orange500
+                    : Colors.white.withOpacity(0.6),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Text(
+                  title,
+                  style: TextStyle(
+                    letterSpacing: .3,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w200,
+                    color: isSelected
+                        ? AppColors.orange500
+                        : Colors.white.withOpacity(0.8),
+                  ),
+                ),
+              )
+            ],
+          )),
+    );
+  }
+}
---
+++ lib/src/drawer/my_drawer.dart
@@ -0,0 +1,83 @@
+import 'package:elden_ring_quest_guide/src/view/home/home.dart';
+import 'package:elden_ring_quest_guide/src/view/npc_list/npc_list_view.dart';
+import 'package:flutter/material.dart';
+
+import '../app_colors.dart';
+import '../app_assets.dart';
+import 'drawer_tile.dart';
+
+class MyDrawer extends StatelessWidget {
+  final String currentPage;
+
+  const MyDrawer({required this.currentPage, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        child: Container(
+      color: AppColors.blue800,
+      child: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+            width: MediaQuery.of(context).size.width * 0.85,
+            child: SafeArea(
+              bottom: false,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 24, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.baseline,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(image: AppImages.placeHolder )
+                        ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10.0),
+                      child: IconButton(
+                          icon: const Icon(Icons.close,
+                              color: AppColors.white, size: 24.0),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          }),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+              flex: 2,
+              child: ListView(
+                padding: const EdgeInsets.only(top: 36, left: 8, right: 16),
+                children: [
+                  DrawerTile(
+                    title: 'Home',
+                    icon: Icons.house,
+                    onTap: () {
+                      if (currentPage != 'home') {
+                        Navigator.pushReplacementNamed(context, Home.routName);
+                      }
+                    },
+                    isSelected: currentPage == 'Home',
+                  ),
+                  DrawerTile(
+                    title: 'NPCs',
+                    icon: Icons.people,
+                    onTap: () {
+                      if (currentPage != 'NPCs') {
+                        Navigator.pushReplacementNamed(context, NpcListView.routeName);
+                      }
+                    },
+                    isSelected: currentPage == 'NPCs',
+                  )
+                ],
+              ))
+        ],
+      ),
+    ));
+  }
+}
