fb26294711d1254c492f3f28cd433ea6893604d8
Guilherme de Massena Ladario <31376051+Guila767@users.noreply.github.com>
2022-05-01 23:06:09 -0300

    home page widget

---
+++ lib/src/view/home/home.dart
@@ -0,0 +1,27 @@
+import 'package:elden_ring_quest_guide/src/drawer/my_drawer.dart';
+import 'package:elden_ring_quest_guide/src/view/settings/settings_view.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({ Key? key }) : super(key: key);
+
+  static const String routName = '/home';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {
+              Navigator.restorablePushNamed(context, SettingsView.routeName);
+            },
+          )
+        ],
+      ),
+      drawer: const MyDrawer(currentPage: 'Home'),
+    );
+  }
+}
