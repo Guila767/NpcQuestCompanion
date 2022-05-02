339a199286cf70b5be3315d4b7710e418961610a
Guilherme de Massena Ladario <31376051+Guila767@users.noreply.github.com>
2022-05-01 22:58:56 -0300

    update app resources

---
+++ lib/src/app_assets.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+class AppImages {
+
+  static ImageProvider placeHolder = const NetworkImage('https://via.placeholder.com/200');
+
+  static String art0 = 'assets/images/artwork0.png';
+  static String art1 = 'assets/images/artwork1.png';
+  static String art2 = 'assets/images/artwork2.png';
+  static String art3 = 'assets/images/artwork3.png';
+
+}
+
+class LottieImages {
+
+  static String loading = 'assets/lottie/loading.json';
+
+}
---
+++ lib/src/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFFE9E9E9);
+
+  static const Color blue600 = Color(0xff4A6572);
+  static const Color blue700 = Color(0xff344955);
+  static const Color blue800 = Color(0xff232F34);
+
+  static const Color orange500 = Color(0xffF9AA33);
+  static const Color orange = Color.fromRGBO(249, 99, 50, 1.0);
+
+  static const Color white = Color(0xffFFFFFF);
+}
