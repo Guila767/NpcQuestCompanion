8184daa039752e834b94e16b131778fe25b468b3
Guilherme de Massena Ladario <31376051+Guila767@users.noreply.github.com>
2022-05-03 19:35:31 -0300

    updated scrapper
    - New data model for the updated scrapped data

--- lib/src/model/npc_model.dart
+++ lib/src/model/npc_model.dart
@@ -6,17 +6,17 @@ part 'npc_model.g.dart';
 @JsonSerializable()
 class NpcModel extends repo.DataModel<NpcModel> {
   
-  final String img;
+  @JsonKey(name: 'name')
   final String name;
-  @JsonKey(name: 'Location')
-  final String? location;
-  @JsonKey(name: 'Role')
-  final String? role;
-  @JsonKey(name: 'Voiced by')
-  final String? voicedBy;
+  @JsonKey(name: 'infoTable')
+  final InfoTable infoTable;
+  @JsonKey(name: 'shopTable')
+  final List<List<String>>? shopTable;
+  @JsonKey(name: 'mainContent')
+  final String mainContent;
   
-  NpcModel({required this.img, required this.name, required this.location, required this.role, this.voicedBy});
-  NpcModel._() : this(img: '', name: '', location: '', role:'');
+  NpcModel({required this.name, required this.infoTable, this.shopTable, required this.mainContent});
+  NpcModel._() : this(name: '', infoTable: InfoTable.empty, mainContent: '');
 
   @override
   Object? get id => name.hashCode;
@@ -30,7 +30,7 @@ class NpcModel extends repo.DataModel<NpcModel> {
   static NpcModel get getInstance => NpcModel._();
   
   @override
-  List<Object?> get props => [name, location, role, voicedBy];
+  List<Object?> get props => [name, mainContent];
 
   @override
   bool? get stringify => true;
@@ -39,4 +39,28 @@ class NpcModel extends repo.DataModel<NpcModel> {
     fromJson: _$NpcModelFromJson,
     toJson: _$NpcModelToJson
   );
+}
+
+@JsonSerializable()
+class InfoTable {
+
+  static InfoTable get empty => InfoTable._();
+  
+  @JsonKey(name: 'name')
+  final String name;
+  @JsonKey(name: 'img')
+  final String img;
+  @JsonKey(name: 'Location')
+  final String? location;
+  @JsonKey(name: 'Role')
+  final String? role;
+  @JsonKey(name: 'Voiced by')
+  final String? voicedBy;
+
+  InfoTable({ required this.name, required this.img, this.location, this.role, this.voicedBy});
+  InfoTable._() : this(name: '', img: '');
+
+  factory InfoTable.fromJson(Map<String, dynamic> json) => _$InfoTableFromJson(json);
+
+  Map<String, dynamic> toJson() => _$InfoTableToJson(this);
 }
--- lib/src/model/npc_model.g.dart
+++ lib/src/model/npc_model.g.dart
@@ -7,16 +7,32 @@ part of 'npc_model.dart';
 // **************************************************************************
 
 NpcModel _$NpcModelFromJson(Map<String, dynamic> json) => NpcModel(
-      img: json['img'] as String,
       name: json['name'] as String,
+      infoTable: InfoTable.fromJson(json['infoTable'] as Map<String, dynamic>),
+      shopTable: (json['shopTable'] as List<dynamic>?)
+          ?.map((e) => (e as List<dynamic>).map((e) => e as String).toList())
+          .toList(),
+      mainContent: json['mainContent'] as String,
+    );
+
+Map<String, dynamic> _$NpcModelToJson(NpcModel instance) => <String, dynamic>{
+      'name': instance.name,
+      'infoTable': instance.infoTable,
+      'shopTable': instance.shopTable,
+      'mainContent': instance.mainContent,
+    };
+
+InfoTable _$InfoTableFromJson(Map<String, dynamic> json) => InfoTable(
+      name: json['name'] as String,
+      img: json['img'] as String,
       location: json['Location'] as String?,
       role: json['Role'] as String?,
       voicedBy: json['Voiced by'] as String?,
     );
 
-Map<String, dynamic> _$NpcModelToJson(NpcModel instance) => <String, dynamic>{
-      'img': instance.img,
+Map<String, dynamic> _$InfoTableToJson(InfoTable instance) => <String, dynamic>{
       'name': instance.name,
+      'img': instance.img,
       'Location': instance.location,
       'Role': instance.role,
       'Voiced by': instance.voicedBy,
